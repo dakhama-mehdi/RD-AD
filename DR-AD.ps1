df268c98982e534abe0b67618a2b003512ac351e
Dakhama mehdi <49924401+dakhama-mehdi@users.noreply.github.com>
2021-03-10 00:21:23 +0100

    Update DR-AD.ps1

--- DR-AD.ps1
+++ DR-AD.ps1
@@ -43,7 +43,7 @@ sleep -Seconds 15
 } 
 
 else {
-$array = New-Object -TypeName "System.Collections.ArrayList"
+#$array = New-Object -TypeName "System.Collections.ArrayList"
 $array = [System.Collections.ArrayList]@()
 $array = Import-Csv -Path C:\Test\array.csv
 }
